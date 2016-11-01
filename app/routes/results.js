6144e2908301a6861ce6c7ac67cf78c3b3859009
Gray Hildreth and Stephen Emery <gray+stephen@>
2016-11-01 13:11:37 -0700

    Add test to see if I can get bioguide.

--- app/routes/results.js
+++ app/routes/results.js
@@ -4,8 +4,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(params) {
     var key = config.myApiKey;
+    var bioGuide = "";
     var url  = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey='+key+'&zip=' + params.zip;
       return Ember.$.getJSON(url).then(function(responseJSON) {
+        for (var i = 0; i < responseJSON.results.length; i++) {
+          bioGuide = responseJSON.results[i].bioguide_id;
+          console.log(bioGuide);
+        }
         return responseJSON.results;
     });
   }
