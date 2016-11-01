56493e06bd5c36eed9d2e7f05217ccff536630e1
Gray Hildreth and Stephen Emery <gray+stephen@>
2016-11-01 11:23:13 -0700

    Add bill lookup form and actions.

--- app/routes/bills.js
+++ app/routes/bills.js
@@ -1,4 +1,12 @@
+import config from '../config/environment';
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function(params) {
+    var key = config.myApiKey;
+    var url = 'http://congress.api.sunlightfoundation.com/bills?apikey='+key+'&query=' + params.bill;
+    return Ember.$.getJSON(url).then(function(responseJSON) {
+      return responseJSON.results;
+    });
+  }
 });
