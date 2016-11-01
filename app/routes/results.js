e0bc4ea7cd738c3ec2ade91430eda76edca5dff0
Gray Hildreth and Stephen Emery <gray+stephen@>
2016-11-01 14:46:59 -0700

    Add legislator search to function with zip and state abbreviation.

--- app/routes/results.js
+++ app/routes/results.js
@@ -4,14 +4,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(params) {
     var key = config.myApiKey;
-    var bioGuide = "";
-    var url  = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey='+key+'&zip=' + params.zip;
-      return Ember.$.getJSON(url).then(function(responseJSON) {
-        for (var i = 0; i < responseJSON.results.length; i++) {
-          bioGuide = responseJSON.results[i].bioguide_id;
-          console.log(bioGuide);
-        }
-        return responseJSON.results;
+    var url = "";
+    if (parseInt(params.zip) === isNaN) {
+      url  = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey='+key+'&zip=' + params.zip;
+    } else {
+      url  = 'http://congress.api.sunlightfoundation.com/legislators?apikey='+key+'&state=' + params.zip.toUpperCase();
+    }
+    return Ember.$.getJSON(url).then(function(responseJSON) {
+      return responseJSON.results;
     });
+
+
   }
 });
