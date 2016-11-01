12d9a4b5fd3805608dd073566e5f35e1dd4f3d8d
Gray Hildreth and Stephen Emery <gray+stephen@>
2016-11-01 09:54:12 -0700

    Add apikey and env file.

--- app/routes/results.js
+++ app/routes/results.js
@@ -1,8 +1,10 @@
+import config from '../config/environment';
 import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    var url  = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=a1e999c963064f4b92d8c0ab43c77ed4&zip=' + params.zip;
+    var key = config.myApiKey;
+    var url  = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey='+key+'&zip=' + params.zip;
       return Ember.$.getJSON(url).then(function(responseJSON) {
         return responseJSON.results;
     });
